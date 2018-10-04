503e4067286c31c58dc58e14b8a0fdf10e1e37e5
PWithanage <psathsaranee@gmail.com>
2018-10-04 10:03:58 +0530

    added update info ui

---
+++ update.php
@@ -0,0 +1,123 @@
+`<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Double Submit Cookie Pattern</title>
+        
+        <link rel="stylesheet"  href="./public/css/bootstrap.min.css">        
+        <script src="./public/js/jquery-3.3.1.min.js"></script>
+
+		<style>
+		.footer {
+		   position: fixed;
+		   left: 0;
+		   bottom: 0;
+		   width: 100%;
+		   background-color: #cdced0;
+		   color: black;
+		   text-align: center;
+		}
+		</style>
+    </head>
+    <body>
+        <nav class="navbar navbar-light bg-light">
+            <ul class="nav justify-content-end">
+                <?php 
+                    if(isset($_COOKIE['session_cookie'])) 
+                    {
+                        echo "<li class='nav-item'>
+                                <a class='nav-link active' href='logout.php'>Logout</a>
+							  </li>";
+                    }
+                ?>
+            </ul>
+        </nav>
+		<div class="col-md-6 mx-auto order-12">
+			<div class="card my-5 p-3 shadow">
+				<div class="card-body">
+					<h5 class="card-title text-center">Update your Address</h5>
+					
+									<script>
+                                        //this function will retrive the CSRF token from the CSRF cookie
+                                        function getCookie(cname) 
+                                        {
+                                            var name = cname + "=";
+                                            var decodedCookie = decodeURIComponent(document.cookie);
+                                            var ca = decodedCookie.split(';');
+                                            for(var i = 0; i <ca.length; i++) 
+                                            {
+                                                var c = ca[i];
+                                                while (c.charAt(0) == ' ') 
+                                                {
+                                                    c = c.substring(1);
+                                                }
+                                                if (c.indexOf(name) == 0) 
+                                                {
+                                                    return c.substring(name.length, c.length);
+                                                }
+                                            }
+                                            return "";
+                                        }
+                                        //this function set hidden CSRF input's value in the form 
+                                        function submitForm(oFormElement)
+                                        {
+                                            document.getElementById("csrf_Token").value=getCookie("csrf_token");
+
+                                        }
+                                    </script>
+						
+                                      <?php
+                                        //check whether user loged in or not 
+                                        if(isset($_COOKIE['session_cookie'])) 
+                                        {
+                                            echo "
+                    						<form method='post' action='result.php' onsubmit='submitForm(this);'>
+                                                <!-- CSRF Token -->
+                                                    <input type='hidden' name='csrf_Token' id='csrf_Token' value=''>   
+                                                <!--  -->	
+                                                <div class='form-group row'>
+                                                	<label for='name' class='col-sm-2 col-form-label'>Full Name</label>
+                                                <div class='col-sm-10'>
+                                                    <input type='text' class='form-control' id='name' name='name' placeholder='Full Name' required>
+                                                </div>
+                                                </div>
+                                              
+                                              	<div class='form-group row'>
+                                                    <label for='university' class='col-sm-2 col-form-label'>University</label>
+                                                <div class='col-sm-10'>
+                                                    <input type='text' class='form-control' id='university' name='university' placeholder='University' required>
+                                                </div>
+                                              	</div>
+                    							
+                    							<div class='form-group row'>
+                                                    <label for='degree' class='col-sm-2 col-form-label'>Degree</label>
+                                                <div class='col-sm-10'>
+                                                    <input type='text' class='form-control' id='degree' name='degree' placeholder='Degree' required>
+                                                </div>
+                                              	</div>
+
+                                              	<div class='form-group row'>
+                                                    <label for='year' class='col-sm-2 col-form-label'>Year</label>
+                                                <div class='col-sm-10'>
+                                                    <input type='number' class='form-control' id='year' name='year' placeholder='Year' required>
+                                                </div>
+                                              	</div>
+
+                                                                
+                                                <button type='submit' class='btn btn-primary' >Submit</button>
+                                           </form>";
+                                        }
+                                    ?>
+				</div>
+			</div>
+		</div>								
+		
+        <div class="footer">
+			<p>Cross Site Request Forgery Protection - Double Submit Cookie Pattern  |  IT15010636</p>
+		</div>
+
+	</body>
+</html>
